25153a0ad43d0adee73cc48a97f18118be1c16a7
rogo6 <piotrrogozinski95@gmail.com>
2018-10-22 23:47:19 +0200

    Added UIKit to project

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import '../node_modules/uikit/dist/css/uikit.css'
+import '../node_modules/uikit/dist/js/uikit.js'
 import "./assets/scss/app.scss";
 
 ReactDOM.render(<App />, document.getElementById('root'));
