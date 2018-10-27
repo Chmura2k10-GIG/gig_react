3efa97374876d2d01ccd181f3ac3e24a2b4556d0
KacperMagdziarz <kacper.magdziarz@accenture.com>
2018-10-27 14:34:53 +0200

    Code refactor and cleaning

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
-import React, { Component } from 'react';
-import Register from "./components/Register"
-import Login from "./components/Login"
-import Notifications from 'react-notify-toast';
+import React, { Component } from "react";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Notifications from "react-notify-toast";
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      isRegistered:false,
-      username:''
-    }
-    this.setAuth= this.setAuth.bind(this);
+    this.state = {
+      isRegistered: false,
+      username: ""
+    };
+    this.setAuth = this.setAuth.bind(this);
   }
 
-  setAuth(email){
-    this.setState({ username:email, isRegistered : true })
+  setAuth(email) {
+    this.setState({ username: email, isRegistered: true });
   }
 
   render() {
     const { isRegistered, username } = this.state;
     return (
       <div>
-        <Login email2="xD"></Login>
+        <Notifications />
+        <Login />
       </div>
       // <div>
       //   <Notifications></Notifications>
 
-      //   {isRegistered ? <Login email2={username}></Login>: <Register username={username} isRegistered={this.setAuth}></Register>  }
+      //   {isRegistered ? <Login></Login>: <Register username={username} isRegistered={this.setAuth}></Register>  }
       // </div>
     );
   }
--- src/lib/api.js
+++ src/lib/api.js
@@ -1,24 +1,28 @@
-import axios from 'axios';
+import axios from "axios";
+import Notifications, { notify } from "react-notify-toast";
+
 
 const url = "http://localhost:8080/";
 
-class Api{
-    constructor(){
-        this.api = axios.create({
-            baseURL: url
-        });
-    }
+class Api {
+  constructor() {
+    this.api = axios.create({
+      baseURL: url
+    });
+  }
 
-    setHeaders(){
-        this.api.defaults.headers = {
-            'Content-Type': 'application/json'
-        };
-    }
+  setHeaders() {
+    this.api.defaults.headers = {
+      "Content-Type": "application/json"
+    };
+  }
+
+  postLogin(userData) {
+    this.setHeaders();
+    this.api.post("users/login", userData).then(res => console.log);
     
-    postLogin(userData){
-        this.setHeaders();
-        this.api.post('users/login', userData).then(res => console.log);
-    }
+    console.log("success");
+  }
 }
 
 export default Api;
